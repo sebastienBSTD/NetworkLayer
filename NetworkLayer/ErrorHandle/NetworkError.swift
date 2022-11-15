168b369c0fab8dbf41a3f86f45ba5d12b2f59342
PierreMaddio <pmsymf@gmail.com>
2022-11-15 20:59:19 +0100

    adding ErrorHandle

---
+++ NetworkLayer/ErrorHandle/NetworkError.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkError.swift
+//  NetworkLayer
+//
+//  Created by Pierre on 15/11/2022.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+        case badUrl, serverError
+        
+        var title: String {
+            switch self {
+            case .badUrl:
+                return "Alert URL"
+            case .serverError:
+                return "Alert Network"
+            }
+        }
+        
+        var message: String {
+            switch self {
+            case .badUrl:
+                return "[🔥] Bad URL"
+            case .serverError:
+                return "[🔥] Server Error"
+            }
+        }
+    }
