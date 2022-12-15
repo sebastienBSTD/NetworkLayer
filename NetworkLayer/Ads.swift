b24eb7a80e3c09586e9d32e2901624e54657b3f7
NIcolas Demange <nicolas.demange@yahoo.fr>
2022-12-15 14:18:18 +0100

    Correction of Ads and Items Struct

---
+++ NetworkLayer/Ads.swift
@@ -0,0 +1,38 @@
+//
+//  Ads.swift
+//  NetworkLayer
+//
+//  Created by Nicolas Demange on 15/12/2022.
+//
+
+import Foundation
+
+// MARK: - Ad
+struct Ad: Codable {
+    let id, categoryID: Int
+    let title, adDescription: String
+    let price: Int
+    let imagesURL: ImagesURL
+    let creationDate: Date
+    let isUrgent: Bool
+    let siret: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case categoryID = "category_id"
+        case title
+        case adDescription = "description"
+        case price
+        case imagesURL = "images_url"
+        case creationDate = "creation_date"
+        case isUrgent = "is_urgent"
+        case siret
+    }
+}
+
+// MARK: - ImagesURL
+struct ImagesURL: Codable {
+    let small, thumb: String?
+}
+
+typealias Ads = [Ad]
---
+++ NetworkLayer/Items.swift
@@ -0,0 +1,19 @@
+//
+//  Items.swift
+//  NetworkLayer
+//
+//  Created by Nicolas Demange on 15/12/2022.
+//
+
+import Foundation
+
+// MARK: - Item
+struct Item: Codable {
+    let id: Int
+    let name: String
+}
+
+typealias Items = [Item]
+
+
+
