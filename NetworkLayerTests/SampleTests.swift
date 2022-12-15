980a4b1cdcb3a63b9834db1ccd983caa617242a5
NIcolas Demange <nicolas.demange@yahoo.fr>
2022-12-15 12:51:57 +0100

    First commit

--- NetworkLayerTests/SampleTests.swift
+++ NetworkLayerTests/SampleTests.swift
@@ -3,7 +3,6 @@ import XCTest
 final class SampleTests: XCTestCase {
 
     func test_sample() {
-        // This file was created for instantiate the repository
         // You can delete it
     }
 }
